695c3bff491ab8b61e5612cfbefaa0ddb22accc7
AzhaganNJ <azhagannatrajan2006@gmail.com>
2025-11-21 09:24:10 +0530

    initial commit

---
+++ README.md
@@ -0,0 +1,30 @@
+🚀 FastAPI Backend Project
+
+A lightweight, high-performance backend built using FastAPI and Uvicorn, featuring automatic interactive API documentation with Swagger UI
+
+🛠️ Installation
+
+- Create Virtual Environment
+
+```bash
+python -m venv venv
+venv\Scripts\activate
+```
+
+- Install Dependencies
+
+```bash
+pip install -r requirements.txt
+```
+
+- Run the Server
+
+```bash
+uvicorn app.main:app --reload
+```
+
+- Swagger UI
+
+```url
+http://127.0.0.1:8000/docs
+```
---
+++ main.py
@@ -0,0 +1,36 @@
+from fastapi import FastAPI
+from pydantic import BaseModel 
+
+app = FastAPI()
+
+# create a class for the student marks
+class StudentMarks(BaseModel):
+    student_id: str
+    ps: int
+    tech: int
+    english: int
+    lifeskills: int
+
+
+# @ = annotations,decorator
+@app.get("/test")
+def greet():
+    return {"hello": "world!"}
+
+
+# get all marks
+@app.get("/marks")
+def get_marks():
+    return {"message":"This end point will return all the marks"}
+
+# get mark by id
+@app.get("/marks/{student_id}")
+def get_marks_by_id(student_id):
+    print(student_id)
+    return {"message": f"This end point will return the marks with student no.{student_id}"}
+
+# create marks 
+@app.post("/marks")
+def create_marks(new_marks: StudentMarks):
+    print(new_marks)
+    pass
